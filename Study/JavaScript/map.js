9a4819df085c63827a367aea27c80e58b0d4e0e9
kiryuchi10 <kaminer_sol2@naver.com>
2025-04-09 05:06:55 +0900

    250409 commit
    study done about closure and study plan

---
+++ Study/JavaScript/map.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {*} arr 
+ * @param {*} fn 
+ * @returns 
+ */
+var map = (arr,fn){
+    let result = [];
+    for (let i = 0; i < arr.length; i++){
+        result.push(fn(arr[i],i))
+    }
+    return result;
+}
